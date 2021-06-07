6f657c5c3913531aae90f8bc962442331c1526f5
Giorginio <jorgehg0909@gmail.com>
2021-06-07 14:05:54 +0200

    Commit recu ej 3

---
+++ Recuperacion2/src/SumaArray.java
@@ -0,0 +1,28 @@
+/**
+ * Se crea una clase
+ * se crea un array
+ * se rellena el array
+ */
+
+import java.util.*;
+
+public class SumaArray {
+
+int[] lista = new int[5];{
+		
+		for (int i = 0; i < lista.length; i++) {
+			lista[0] = 1;
+			lista[1] = 2;
+			lista[2] = 3;
+			lista[3] = 4;
+			lista[4] = 5;
+			lista[5] = 6;
+			
+		}
+		
+	public void sumaArray(int[] lista) {
+		
+	}
+
+	}
+}
